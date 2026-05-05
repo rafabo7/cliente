b0932d445a272d8c5b153c8f78eb5ee45da53a11
Rafael Botella Sánchez <122386430+rafabo7@users.noreply.github.com>
2026-05-05 11:07:11 +0200

    refact: procesarUsuario sin handler then() para mayor claridad

--- ex-2ev/main.js
+++ ex-2ev/main.js
@@ -1,34 +1,31 @@
 import { obtenerDatosUsuario, obtenerEstadisticas } from './userlib.js'
 
 async function procesarUsuario(userId) {
-    
-    return Promise.all([
-        obtenerDatosUsuario(userId), 
+
+    const [datos, estadisticas] = await Promise.all([
+        obtenerDatosUsuario(userId),
         obtenerEstadisticas(userId)
-    ]).then( ([datos, estadisticas]) => {
+    ])
 
-        return {
-            ...datos,
-            estadisticas
-        }
-
+    return {
+        ...datos,
+        estadisticas
     }
-)
 
 }
 
 async function procesarUsuarios(arr) {
     let data = []
     for (let i = 0; i < arr.length; i++) {
-        data.push( procesarUsuario(arr[i]))
-        
+        data.push(procesarUsuario(arr[i]))
+
     }
     return Promise.all(data)
 }
 
 let user2 = await procesarUsuario(2)
-// console.log(user2)
+console.log(user2)
 
-let userData = await procesarUsuarios([ 3, 2, 1 ])
+let userData = await procesarUsuarios([3, 2, 1])
 
 console.log(userData)
